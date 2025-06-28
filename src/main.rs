6aa3de002fa0f38dc2b54f637840711c070de32b
Yves Ineichen <iff@yvesineichen.com>
2025-06-28 07:50:19 +0200

    ugly cat-file

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::Read;
+use std::io::Write;
+
+use flate2::read::ZlibDecoder;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     eprintln!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
     if args[1] == "init" {
         fs::create_dir(".git").unwrap();
@@ -15,6 +20,40 @@ fn main() {
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
         println!("Initialized git directory")
+    } else if args[1] == "cat-file" {
+        // args: -p <blob_sha>
+        // TODO commands
+        assert!(args[2] == "-p");
+
+        // read file .git/objects/2chars/rest
+        let hash = &args[3];
+        let file = std::fs::File::open(format!(".git/objects/{}/{}", &hash[..2], &hash[2..]))
+            .expect("object exists");
+
+        let z = ZlibDecoder::new(file);
+        let mut r = BufReader::new(z);
+
+        // extract content: blob <size>\0<content>
+        let mut header = Vec::new();
+        r.read_until(0, &mut header).expect("0 in header");
+        // TODO convert this to a string
+        // is it utf8? 16..?
+        let content = str::from_utf8(header.as_slice()).expect("content");
+
+        let Some((_blob_type, size)) = content.split_once(' ') else {
+            return;
+        };
+        // TODO check that we get a 'blob'
+
+        let size = size.parse::<u64>().expect("parsable size");
+        let mut content = Vec::new();
+        r.take(size)
+            .read_to_end(&mut content)
+            .expect("reading content of size");
+        let content = str::from_utf8(content.as_slice()).expect("content is string");
+        let stdout = std::io::stdout();
+        let mut stdout = stdout.lock();
+        write!(stdout, "{}", content).expect("writing to stdout");
     } else {
         println!("unknown command: {}", args[1])
     }
