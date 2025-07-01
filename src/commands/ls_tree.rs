5c0e406e3a5e9e47d34cda49cb4a9b75d57f093b
Yves Ineichen <iff@yvesineichen.com>
2025-07-01 22:08:32 +0200

    hacky ls-tree

--- src/commands/ls_tree.rs
+++ src/commands/ls_tree.rs
@@ -2,6 +2,34 @@ use crate::object::to_stdout;
 use crate::object::Object;
 
 pub(crate) fn invoke(name_only: bool, hash: &str) -> anyhow::Result<()> {
-    // to_stdout(content.to_string())?;
+    let object = Object::from_hash(hash)?;
+    // TODO sort
+    let content = object.tree_content()?;
+    if name_only {
+        to_stdout(
+            content
+                .iter()
+                .map(|t| t.name.clone())
+                .collect::<Vec<String>>()
+                .join("\n"),
+        )?;
+    } else {
+        to_stdout(
+            content
+                .iter()
+                .map(|t| {
+                    // FIXME object type
+                    // FIXME sha
+                    format!(
+                        "{} blob {}\t{}",
+                        t.mode,
+                        String::from("sha"),
+                        t.name.clone()
+                    )
+                })
+                .collect::<Vec<String>>()
+                .join("\n"),
+        )?;
+    }
     Ok(())
 }
