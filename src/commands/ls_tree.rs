1e05fb2c485a69d2cfdbd6ffe561818b9215bdb7
Yves Ineichen <iff@yvesineichen.com>
2025-07-03 08:48:28 +0200

    ls_tree: cleanup

--- src/commands/ls_tree.rs
+++ src/commands/ls_tree.rs
@@ -3,7 +3,6 @@ use crate::object::Object;
 
 pub(crate) fn invoke(name_only: bool, hash: &str) -> anyhow::Result<()> {
     let object = Object::from_hash(hash)?;
-    // TODO sort
     let content = object.tree_content()?;
     if name_only {
         to_stdout(
@@ -13,18 +12,25 @@ pub(crate) fn invoke(name_only: bool, hash: &str) -> anyhow::Result<()> {
                 .collect::<Vec<String>>()
                 .join("\n"),
         )?;
-        to_stdout("\n".to_string())?;
     } else {
         to_stdout(
             content
                 .iter()
                 .map(|t| {
                     // FIXME object type
-                    format!("{} blob {}\t{}", t.mode, hex::encode(t.sha_bytes), t.name.clone())
+                    format!(
+                        "{} blob {}\t{}",
+                        t.mode,
+                        hex::encode(t.sha_bytes),
+                        t.name.clone()
+                    )
                 })
                 .collect::<Vec<String>>()
                 .join("\n"),
         )?;
     }
+
+    // FIXME why?
+    to_stdout("\n".to_string())?;
     Ok(())
 }
