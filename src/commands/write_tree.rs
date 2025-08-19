a57ed407461044c139cc8409eab5c4f498b7e1fb
Yves Ineichen <iff@yvesineichen.com>
2025-08-19 11:00:10 +0200

    write_tree: correctly sort entries

--- src/commands/write_tree.rs
+++ src/commands/write_tree.rs
@@ -1,14 +1,71 @@
-use std::{fs, io::Write, path::Path};
+use std::{
+    cmp::Ordering,
+    ffi::OsString,
+    fs::{self, DirEntry, Metadata},
+    io::Write,
+    path::Path,
+};
+
+use anyhow::Context;
 
 use crate::object::{to_stdout, GitObjectWriter, Object, ObjectType};
 
 fn write_tree(path: &Path) -> anyhow::Result<Option<[u8; 20]>> {
     let mut tree_objects = Vec::new();
-    for entries in fs::read_dir(path)? {
-        let entry = entries?;
+
+    let mut entries: Vec<(DirEntry, OsString, Metadata)> = Vec::new();
+    for dir_entry in fs::read_dir(path)? {
+        let entry = dir_entry?;
         let file_name = entry.file_name();
         let meta = entry.metadata()?;
+        entries.push((entry, file_name, meta));
+    }
 
+    // explicitly handle directories to include trailing slash when sorting
+    // see `base_name_compare` in `tree.c`
+    entries.sort_unstable_by(|a, b| {
+        let aa = a.1.as_encoded_bytes();
+        let bb = b.1.as_encoded_bytes();
+        let prefix_len = std::cmp::min(aa.len(), bb.len());
+
+        // should match memcmp(name1, name2, len)
+        // only continue if strings are the same on the shared length
+        match aa[..prefix_len].cmp(&bb[..prefix_len]) {
+            Ordering::Equal => {
+                // handle this case here but would also be correct otherwise
+                // comparing \0 with \0 below
+                if aa.len() == bb.len() {
+                    return Ordering::Equal;
+                }
+                // else continue
+            }
+            o => return o,
+        }
+
+        // otherwise check next character
+        // the directory special case: git stores dirs without trailing slashes but for the purpose
+        // of sorting they should be treated as if they have a trailing / to ensure lexicographic
+        // ordering (test/ comes before test.txt)
+        let c1 = if let Some(c) = aa.get(prefix_len).copied() {
+            c
+        } else if a.2.is_dir() {
+            b'/'
+        } else {
+            b'\0'
+        };
+
+        let c2 = if let Some(c) = bb.get(prefix_len).copied() {
+            c
+        } else if b.2.is_dir() {
+            b'/'
+        } else {
+            b'\0'
+        };
+
+        c1.cmp(&c2)
+    });
+
+    for (entry, file_name, meta) in entries {
         if file_name == ".git" {
             continue;
         }
@@ -21,16 +78,16 @@ fn write_tree(path: &Path) -> anyhow::Result<Option<[u8; 20]>> {
 
         let hash = if meta.is_dir() {
             // If the entry is a directory, recursively create a tree object and record its SHA-1 hash
+            // handle empty directory here
             let Some(hash) = write_tree(&path)? else {
-                // empty directory, so don't include in parent
                 continue;
             };
             hash
         } else {
             // If the entry is a file, create a blob object and record its SHA-1 hash
-            let o = Object::from_path(path.to_str().expect("path must be valid"))?;
-            o.write()?;
-            o.hash()
+            let blob = Object::from_path(path.to_str().context("getting path as string")?)?;
+            blob.write()?;
+            blob.hash()
         };
 
         tree_objects.extend(mode.as_bytes());
@@ -59,6 +116,7 @@ fn write_tree(path: &Path) -> anyhow::Result<Option<[u8; 20]>> {
 pub(crate) fn invoke() -> anyhow::Result<()> {
     // we won't implement a staging area
     // just assume that all files in the working directory are staged.
+    // TODO error if empty?
     if let Some(hash) = write_tree(Path::new("."))? {
         to_stdout(hex::encode(hash))?;
     }
