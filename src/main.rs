c2cdec2f93be71aeb23bf7b22fbac3f5cc33ae6b
Yves Ineichen <iff@yvesineichen.com>
2025-08-19 15:56:45 +0200

    commit_tree: command

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,14 @@ enum Command {
         #[arg(action)]
         hash: String,
     },
+    CommitTree {
+        #[arg(short = 'p')]
+        parent: Option<String>,
+        #[arg(short = 'm')]
+        message: String,
+        #[arg(action)]
+        tree: String,
+    },
     HashObject {
         #[arg(short = 'w')]
         write: bool,
@@ -40,6 +48,11 @@ fn main() -> anyhow::Result<()> {
 
     match args.command {
         Command::CatFile { pretty, hash } => commands::cat_file::invoke(pretty, &hash),
+        Command::CommitTree {
+            parent,
+            message,
+            tree,
+        } => commands::commit_tree::invoke(&message, parent, &tree),
         Command::Init {} => commands::init::invoke(),
         Command::HashObject { write, path } => commands::hash_object::invoke(write, &path),
         Command::LsTree { name_only, hash } => commands::ls_tree::invoke(name_only, &hash),
