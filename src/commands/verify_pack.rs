bde0ccbdd3610d6caebf91ec2bee0a6da302a67b
Yves Ineichen <iff@yvesineichen.com>
2025-09-09 16:33:23 +0200

    verify_pack: cleanup

--- src/commands/verify_pack.rs
+++ src/commands/verify_pack.rs
@@ -9,11 +9,11 @@ pub(crate) fn invoke(pack_file: &str, verbose: bool) -> anyhow::Result<()> {
     let file = std::fs::File::open(pack_file)?;
     let mut reader = BufReader::new(file);
     let mut data = Vec::new();
-    let _read_size = reader.read_to_end(&mut data)?;
+    reader.read_to_end(&mut data)?;
 
     let mut offset = 0;
     let (rest, (version, num_objects)) =
-        pack::parse_header(&data).map_err(|e| anyhow::anyhow!("Failed to parse pack: {:?}", e))?;
+        pack::parse_header(&data).map_err(|e| anyhow::anyhow!("failed to parse pack: {:?}", e))?;
     assert!(version == 2);
     offset += data.len() - rest.len();
 
@@ -21,7 +21,7 @@ pub(crate) fn invoke(pack_file: &str, verbose: bool) -> anyhow::Result<()> {
     let mut rest = rest;
     for _ in 0..num_objects {
         let (new_rest, (object_type, length)) = pack::parse_object_header(rest)
-            .map_err(|e| anyhow::anyhow!("Failed to parse pack object header: {:?}", e))?;
+            .map_err(|e| anyhow::anyhow!("failed to parse pack object header: {:?}", e))?;
         let (new_rest, entry) = pack::parse_object(object_type, length, new_rest);
         pack_objects.insert(offset, entry);
 
